c916b02dc4455fca26474dfe1b5fe69c8cb96bd1
sdim-ua <ingersol@yandex.ua>
2019-02-03 23:02:29 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/* 
+Исправляем ошибки юности
+*/
+
+public class Solution {
+    public static int max = 100;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String text = "The max is ";
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        max = a > b ? a : b;
+
+        System.out.println(text + max);
+    }
+
+}
